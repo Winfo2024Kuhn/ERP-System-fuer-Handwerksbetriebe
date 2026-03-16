4eff45a327025da31f3c227734d2c9e63a8a0389
Winfo2024Kuhn <marvin.kuhn@study.thws.de>
2026-03-16 12:54:16 +0100

    Add new JavaScript file for enhanced functionality in static assets

--- src/main/java/org/example/kalkulationsprogramm/repository/LieferantReklamationRepository.java
+++ src/main/java/org/example/kalkulationsprogramm/repository/LieferantReklamationRepository.java
@@ -1,6 +1,7 @@
 package org.example.kalkulationsprogramm.repository;
 
 import org.example.kalkulationsprogramm.domain.LieferantReklamation;
+import org.example.kalkulationsprogramm.domain.ReklamationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,6 @@ import java.util.List;
 @Repository
 public interface LieferantReklamationRepository extends JpaRepository<LieferantReklamation, Long> {
     List<LieferantReklamation> findByLieferantIdOrderByStatusAscErstelltAmDesc(Long lieferantId);
+
+    List<LieferantReklamation> findByStatusOrderByErstelltAmDesc(ReklamationStatus status);
 }
